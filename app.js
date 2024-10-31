7a8dc09e78c77d3ea13507f680e0e5bd87d5e9a4
Kaan Latinler <kaanlatinlerhd@gmail.com>
2024-10-31 22:43:39 +0300

    last shots

--- app.js
+++ app.js
@@ -16,7 +16,13 @@ const userRoutes = require("./src/routes/user/userRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 app.use(express.json());
 
 app.use((req, res, next) => {
