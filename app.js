18449f1b122146ebcdc34204b222359c83caf937
Kaan Latinler <kaanlatinlerhd@gmail.com>
2024-10-31 22:48:31 +0300

    son son

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'https://www.prekastev.com',
+    origin: "*",
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization",
   })
